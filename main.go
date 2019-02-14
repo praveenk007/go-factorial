5c5b0bb46967819ce9c97cbed65dc51247fa0da5
pk1992 <praveen.k@gmail.com>
2019-02-15 00:21:31 +0530

    added basic algo for factorial

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	n := 4
+	//series := make(chan int, n)
+	fmt.Println(fibo(n))
+	/*o fibo_start(series, n)
+	for i := range series {
+		fmt.Println(i)
+	}*/
+}
+
+func fibo_start(series chan int, n int) {
+
+	close(series)
+}
+
+func fibo(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fibo(n-1)
 }
