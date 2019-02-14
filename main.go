af795f00491050cd0e07d04936dbdbb30f0fc7e4
pk1992 <praveen.k@gmail.com>
2019-02-15 00:21:50 +0530

    added basic algo for factorial

--- main.go
+++ main.go
@@ -7,21 +7,21 @@ import (
 func main() {
 	n := 4
 	//series := make(chan int, n)
-	fmt.Println(fibo(n))
+	fmt.Println(fact(n))
 	/*o fibo_start(series, n)
 	for i := range series {
 		fmt.Println(i)
 	}*/
 }
 
-func fibo_start(series chan int, n int) {
+func fact_start(series chan int, n int) {
 
 	close(series)
 }
 
-func fibo(n int) int {
+func fact(n int) int {
 	if n == 0 {
 		return 1
 	}
-	return n * fibo(n-1)
+	return n * fact(n-1)
 }
